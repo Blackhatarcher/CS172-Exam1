9ff376f462bc08810ef44eba06ec18653b34054a
christianwilliams20 <christianwilliams20@LAB-SCI-214-07>
2016-10-06 15:10:19 -0700

    finished theatre.h

--- Exam1/ConsoleApplication1/Theatre.h
+++ Exam1/ConsoleApplication1/Theatre.h
@@ -1,8 +1,20 @@
 #pragma once
+#include <string>
+#include "Movie.h"
 class Theatre
 {
+private:
+	std::string name;
+	std::string phone;
+	int popcorn_price;
+	int coke_price;
+	Movie movies[];
 public:
-	Theatre();
-
+	Theatre(std::string Name, std::string Phone);
+	void AddMovie(Movie& Movie);
+	std::string GetMovieForHour(int Hour);
+	int GetShowTimeForGenre(std::string Genre);
+	int GetPopcornPrice();
+	int GetCokePrice();
 };
 
